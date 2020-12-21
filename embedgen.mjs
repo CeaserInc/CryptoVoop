88b2d7b7a3aa01b9a194161d5846246d2c9697d5
CeaserInc <76456793+CeaserInc@users.noreply.github.com>
2020-12-21 17:40:31 -0600

    Add files via upload

---
+++ embedgen.mjs
@@ -0,0 +1,23 @@
+import Discord from 'discord.js';
+
+// Start of module
+
+// Generate embed
+function generateEmbed(color, title, authorName, authorLink, authorImg, description, footerText, footerImg) {
+	// Create embed using parameters
+    var generatedEmbed = new Discord.MessageEmbed()
+		.setColor(color)
+		.setTitle(title)
+		.setAuthor(authorName, authorImg, authorLink)
+		.setDescription(description)
+		.setTimestamp()
+		.setFooter(footerText, footerImg);
+		
+	// Return generated embed
+    return generatedEmbed;
+}
+
+// Export necessary functions
+export {
+    generateEmbed
+};
